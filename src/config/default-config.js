76b3b652acec877dba4b7eb2eba464c5244c24ca
Felipe De Andrade <felipe.deam98@gmail.com>
2026-03-11 08:22:53 -0300

    chore(config): add config files

---
+++ src/config/default-config.js
@@ -0,0 +1,15 @@
+// Default configuration
+
+const defaultConfig = {
+  inputDir: "src/assets/svg/icons-ui",
+  outputDir: "dist/assets/fonts/Epiled",
+  font: {
+    fontName: "Epiled",
+    folderName: "Epiled",
+    fontFileName: "epiled",
+    fontPath: "../assets/fonts",
+  },
+  cssClass: "icon-",
+};
+
+export { defaultConfig };
